fe29aa458aa02ef6601c5af3f88a92997baef2db
waki285 <suzuneu@suzuneu.com>
2023-12-09 21:01:08 +0900

    np

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 mod func;
 
-use std::io::{stdout, Write};
+use std::{io::{stdout, Write}, sync::{Arc, RwLock}};
 
 use crate::func::{get_trip, randobet};
 
@@ -26,14 +26,21 @@ fn main() {
 
     let start_time = std::time::Instant::now();
 
+    let stop_flag = Arc::new(RwLock::new(false));
+
     for _ in 0..threads {
         let word = word.clone();
         let word = word.trim();
         let word = word.to_string();
+        let stop_flag = Arc::clone(&stop_flag);
         std::thread::spawn(move || {
             let mut i: usize = 0;
             let mut key = String::new();
             loop {
+                let h = stop_flag.read().unwrap();
+                if *h {
+                    break;
+                }
                 if i == 0 {
                     i = 1;
                     key = randobet(2, Some("./"));
@@ -55,6 +62,9 @@ fn main() {
                 let trip = &trip[trip.len() - 10..];
 
                 if trip.starts_with(&word) {
+                    let mut h = stop_flag.write().unwrap();
+                    *h = true;
+                    std::thread::sleep(std::time::Duration::from_millis(100));
                     println!("Found!");
                     println!("Trip: {}", trip);
                     println!("Key: #{}00", final_key);
